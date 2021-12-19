aac8763c282c1f42fdc15805989e26038b4eca30
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2021-12-19 23:13:35 +0100

    add UI
    Added scalable UI with buttons for every bus and tram

--- app/src/main/java/PublicTransportFinder/view/HomeController.java
+++ app/src/main/java/PublicTransportFinder/view/HomeController.java
@@ -2,6 +2,7 @@ package PublicTransportFinder.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -12,11 +13,27 @@ public class HomeController {
     @FXML
     private void initialize(){
         engine = map.getEngine();
-        engine.load(getClass().getResource("googleMaps.html").toString());
+        engine.load(getClass().getClassLoader().getResource("googleMaps.html").toString());
     }
 
     @FXML
     private void setTerr(ActionEvent actionEvent) {
-        engine.executeScript("document.setMapTypeHybrid()");
+        engine.executeScript("document.addBusMarker(51.105286, 17.079487, 13)");
+    }
+
+    @FXML
+    private void deleteMarkers(ActionEvent actionEvent) {
+        engine.executeScript("document.clearAllBusTramMarkers()");
+    }
+
+    @FXML
+    private void addBusTracker(ActionEvent actionEvent) {
+        String buttonId = ((Node) actionEvent.getSource()).getId();
+        System.out.println(buttonId);
+    }
+
+    public void addTramTracker(ActionEvent actionEvent) {
+        String buttonId = ((Node) actionEvent.getSource()).getId();
+        System.out.println(buttonId);
     }
 }
